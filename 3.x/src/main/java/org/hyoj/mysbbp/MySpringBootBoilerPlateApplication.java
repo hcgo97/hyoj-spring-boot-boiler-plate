0cbd8d8d72f7193313bd30eed3895141de13838b
DESKTOP-RE0B9RR\hyoj <gywjd5251@gmail.com>
2024-02-06 10:46:20 +0900

    :tada: init: Spring Boot 3.x 버전을 위한 패키지 및 소스 추가

---
+++ 3.x/src/main/java/org/hyoj/mysbbp/MySpringBootBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package org.hyoj.mysbbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootBoilerPlateApplication.class, args);
+	}
+
+}
